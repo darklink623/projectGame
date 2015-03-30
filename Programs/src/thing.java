28683d16d7b8df14788592e80d04a22a217776cb
pi6286 <pi62866@gmail.com>
2015-03-30 17:47:10 -0400

    Adding my project directory. It'll just let me work on my projects on any computer and it'll let you guys try it out if you ever feel like it.

---
+++ Programs/src/thing.java
@@ -0,0 +1,10 @@
+/**
+ * Created by elijah on 3/30/15.
+ */
+public class thing
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is just a test program that doesn't do anything useful.");
+    }
+}
